255dcb852bcdb08831063eddd3f7e516b58fe8a6
kellycypher98 <kelvinboateng94@outlook.com>
2024-01-08 23:03:06 +0000

    little changes made

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       options: {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
+          favicon: "./content/images/DALL·E 2023-05-26 23.35.57.png", // Path is relative to the root
           siteName: "My Minimal Portfolio", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
